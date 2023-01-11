1dcd2cc360b63607c02b72a8d4bddcfbde2a525c
xvzc <dev.kwanghoo@gmail.com>
2023-01-11 18:09:13 +0900

    feat: Make structure

--- lua/skeleton/core.lua
+++ lua/skeleton/core.lua
@@ -1,7 +1,19 @@
 local M = {}
 
-M.datetime = function()
+M.default_author = function()
+  os.execute('git config user.name | whoami')
+end
+
+M.default_datetime = function()
     return os.date("%Y-%m-%d %H:%M:%S")
+end
+
+M.contains = function(table, key)
+  return table[key] ~= nil
+end
+
+M.load_template = function(file_path, config)
+  print(file_path)
 end
 
 return M
--- lua/skeleton/init.lua
+++ lua/skeleton/init.lua
@@ -2,13 +2,31 @@ local core = require('skeleton.core')
 
 local M = {}
 
+M.config = {}
+
 M.test = function()
   print('test okay')
-  print(core.datetime())
+  print(core.get_datetime())
 end
 
 M.setup = function(config)
-  M.tags = config.tags
+  if not core.contains(config.tags, "timestamp") then
+    config.tags.timestamp = core.default_datetime
+  end
+
+  if not core.contains(config.tags, "author") then
+    config.tags.timestamp = core.default_author
+  end
+
+  M.config = config
+end
+
+M.load = function(file_path, config)
+  if config ~= nil then
+    core.load_template(file_path, config)
+  else
+    core.load_template(file_path, M.config)
+  end
 end
 
 return M
