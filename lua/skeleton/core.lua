00d7b5f8c17f9b1a13359e48c5d85c2b058d552e
xvzc <dev.kwanghoo@gmail.com>
2023-01-11 17:31:47 +0900

    chore: test

---
+++ lua/skeleton/core.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.datetime = function()
+    return os.date("%Y-%m-%d %H:%M:%S")
+end
+
+return M
--- lua/skeleton/init.lua
+++ lua/skeleton/init.lua
@@ -1,7 +1,14 @@
+local core = require('core')
+
 local M = {}
 
 M.test = function()
   print('test okay')
+  print(core.datetime())
+end
+
+M.setup = function(config)
+  M.tags = config.tags
 end
 
 return M
